41694530926ef062335abad6226798dda303d477
donghyun771 <donghyun761@gmail.com>
2022-04-28 16:08:59 +0900

    사칙연산 출력 추가

--- ImtStudy/src/Arithmetic_Operation.java
+++ ImtStudy/src/Arithmetic_Operation.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public interface Arithmetic_Operation {
+public interface Arithmetic_Operation
+{
+    public static void Aithmetic_Operation(int num1, int num2) {
+
+        System.out.println(num1 + " + " +num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " +num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " / " +num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " * " +num2 + " = " + (num1 * num2));
+
+    }
+
+    public static int Aithmetic_Operation(int num1, int num2, String str1) {
+        if (str1.equals("+")) {
+
+            int res = num1 + num2;
+            return res;
+
+        } else if (str1.equals("-")) {
+
+            int res = num1 - num2;
+            return res;
+
+        } else if (str1.equals("*")) {
+
+            int res = num1 * num2;
+            return res;
+
+        } else if (str1.equals("/")) {
+
+            int res = num1 / num2;
+            return res;
+
+        }else {
+            System.out.println("잘못된 입력입니다.");
+        }
+        return 0;
+    }
+
 }
+
--- ImtStudy/src/Main.java
+++ ImtStudy/src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main implements Arithmetic_Operation{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        Arithmetic_Operation.Aithmetic_Operation(num1, num2);
     }
 }
